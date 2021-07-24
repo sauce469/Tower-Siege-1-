2890b841aa01da43e2aa1f8a103e3feecdff242d
sauce469 <77946314+sauce469@users.noreply.github.com>
2021-07-24 11:42:16 -0700

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(polygon,constraintPoint){
+        var options={
+            bodyA:polygon,
+            pointB:constraintPoint,
+            stiffness:0.04,
+            length:1
+        }
+        this.sling=Constraint.create(options);
+        this.B=constraintPoint;
+        World.add(world,this.sling)
+    }
+    fly(){
+        this.sling.bodyA=null;
+    }
+    display(){
+        if (this.sling.bodyA){
+
+        
+        var A = this.sling.bodyA.position
+        strokeWeight(4); 
+        stroke("turquoise");
+        line(A.x,A.y,this.B.x,this.B.y)
+        }
+    }
+}
